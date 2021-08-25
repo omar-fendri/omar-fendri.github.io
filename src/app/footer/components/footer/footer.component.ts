0c594638c597ce507737d526569e141d9873fc30
Omar Fendri <fendri.omar@yahoo.fr>
2021-08-25 17:46:25 +0100

    import bootstrap-grid & update resume

--- src/app/footer/components/footer/footer.component.ts
+++ src/app/footer/components/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {BreakpointObserver} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  isWideScreen$: Observable<boolean> | undefined;
+  isSmallScreen$: Observable<boolean> | undefined;
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
+    this.isWideScreen$ = this.breakpointObserver.observe(['(min-width: 1200px)']).pipe(map((({matches}) => matches)));
+    this.isSmallScreen$ = this.breakpointObserver.observe(['(max-width: 576px)']).pipe(map((({matches}) => matches)));
   }
 
 }
--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  isWideScreen$: Observable<boolean> | undefined;
+  isSmallScreen$: Observable<boolean> | undefined;
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
+    this.isWideScreen$ = this.breakpointObserver.observe(['(min-width: 1200px)']).pipe(map((({matches}) => matches)));
+    this.isSmallScreen$ = this.breakpointObserver.observe(['(max-width: 576px)']).pipe(map((({matches}) => matches)));
   }
-
+  downloadMyResume() {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'assets/resume.pdf');
+    link.setAttribute('download', `Omar-Fendri-Resume-03-2021.pdf`);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
 }
